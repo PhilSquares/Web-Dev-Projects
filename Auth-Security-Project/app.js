d54aa4d93c713a25669dcf4e068bf6f533d4b904
Phillip Squarcetta <philsquares@Phillips-MacBook-Pro.local>
2023-04-18 10:01:08 -0400

    Level 1 Auth - Register Users with Username and Password via MongoDB database.

--- Auth-Security-Project/app.js
+++ Auth-Security-Project/app.js
@@ -2,12 +2,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const moongoose = require("mongoose");
+const { default: mongoose } = require("mongoose");
 
 const app = express();
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+
+moongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+
+const userSchema = {
+    email: String,
+    password: String
+};
+
+const User = new mongoose.model("User", userSchema);
 
 app.get("/", function(req, res){
     res.render("home");
@@ -19,6 +30,41 @@ app.get("/login", function(req, res){
 
 app.get("/register", function(req, res){
     res.render("register");
+});
+
+app.post("/register", function(req, res){
+    const newUser = new User({
+        //.username comes from the name value in the input tag seen in register.ejs line 14. 
+        //.password comes from the name value in the input tag seen in register.ejs line 18.
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save(function(err){
+        if(err){
+            console.log(err);
+        } else{
+            res.render("secrets");
+        }
+    });
+});
+
+app.post("/login", function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    //Check if the password is equal to the one that the user typed in:
+    User.findOne({email: username}, function(err, foundUser){
+        if(err){
+            console.log(err);
+        } else {
+            if(foundUser){
+                if(foundUser.password === password){
+                    res.render("secrets");
+                }
+            }
+        }
+    });
 });
 
 app.listen(3000, function(){
